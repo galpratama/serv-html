ff1b1ce9a2768676ae0541e6af6f2bcd7897edc1
Galih Pratama <hanamura.iost@gmail.com>
2021-05-21 00:10:39 +0700

    Layout boilerplate

---
+++ public/dashboard/empty/services.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include '../includes/meta.php' ?>
+    <?php include '../includes/styles.php' ?>
+    <title>Serv</title>
+</head>
+
+<body class="antialiased">
+    <div class="relative">
+
+    </div>
+    
+    <?php include '../includes/scripts.php' ?>
+</body>
+
+</html>
