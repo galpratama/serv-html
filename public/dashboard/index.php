7062c67995f45325209f307bbc5765515543b338
Galih Pratama <hanamura.iost@gmail.com>
2021-06-01 19:51:50 +0700

    Dashboard layout

--- public/dashboard/index.php
+++ public/dashboard/index.php
@@ -1,18 +1,35 @@
 <!doctype html>
-<html lang="en">
+<html x-data="data()" lang="en">
 
 <head>
-    <?php include '../includes/meta.php' ?>
-    <?php include '../includes/styles.php' ?>
-    <title>Serv</title>
+    <?php include '../includes/dashboard/meta.php' ?>
+    <?php include '../includes/dashboard/styles.php' ?>
+    <title>Dashboard</title>
 </head>
 
 <body class="antialiased">
-    <div class="relative">
+    <div class="flex h-screen bg-serv-services-bg" :class="{ 'overflow-hidden': isSideMenuOpen }">
+        <?php include '../components/dashboard/menu/desktop.php' ?>
 
+        <div x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
+        <?php include '../components/dashboard/menu/mobile.php' ?>
+
+        <div class="flex flex-col flex-1 w-full">
+            <?php include '../components/dashboard/header.php' ?>
+
+            <main class="h-full overflow-y-auto">
+                <div class="container px-6 mx-auto grid">
+                    <h2 class="mt-6 mb-1 text-2xl font-semibold text-gray-700">
+                        Overviews
+                    </h2>
+                    <p class="text-sm text-gray-400">
+                        Monthly Reports
+                    </p>
+                </div>
+            </main>
+        </div>
     </div>
-    
-    <?php include '../includes/scripts.php' ?>
+    <?php include '../includes/dashboard/scripts.php' ?>
 </body>
 
 </html>
---
+++ public/includes/dashboard/scripts.php
@@ -0,0 +1,2 @@
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+<script src="/js/dashboard/init-alpine.js"></script>
