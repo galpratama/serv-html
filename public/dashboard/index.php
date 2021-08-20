ccdd6a2375704235bba7990bf7beab558b07fa5d
Galih Pratama <hanamura.iost@gmail.com>
2021-08-20 10:58:32 +0700

    Profile icon

--- public/dashboard/index.php
+++ public/dashboard/index.php
@@ -11,20 +11,32 @@
     <div class="flex h-screen bg-serv-services-bg" :class="{ 'overflow-hidden': isSideMenuOpen }">
         <?php include '../components/dashboard/menu/desktop.php' ?>
 
-        <div x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
+        <div x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 flex items-end bg-black bg-opacity-50 z-1 sm:items-center sm:justify-center"></div>
         <?php include '../components/dashboard/menu/mobile.php' ?>
 
         <div class="flex flex-col flex-1 w-full">
             <?php include '../components/dashboard/header.php' ?>
 
             <main class="h-full overflow-y-auto">
-                <div class="container grid px-10 mx-auto">
-                    <h2 class="mt-8 mb-1 text-2xl font-semibold text-gray-700">
-                        Overviews
-                    </h2>
-                    <p class="text-sm text-gray-400">
-                        Monthly Reports
-                    </p>
+                <div class="container mx-auto">
+                    <div class="grid w-full gap-5 px-10 mx-auto md:grid-cols-12">
+                        <div class="col-span-8">
+                            <h2 class="mt-8 mb-1 text-2xl font-semibold text-gray-700">
+                                Overviews
+                            </h2>
+                            <p class="text-sm text-gray-400">
+                                Monthly Reports
+                            </p>
+                        </div>
+                        <div class="col-span-4 text-right">
+                            <div @click.away="open = false" class="relative z-10 hidden mt-5 lg:block" x-data="{ open: false }">
+                                <button class="flex flex-row items-center w-full px-4 py-2 mt-2 text-left bg-white rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4">
+                                <img class="inline w-12 h-12 mr-3 rounded-full" src="https://randomuser.me/api/portraits/men/1.jpg" alt="">
+                                    Halo, Miles
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <section class="container px-6 mx-auto mt-5">
                     <div class="grid gap-5 md:grid-cols-12">
@@ -255,7 +267,7 @@
                                         </tr>
                                     </thead>
                                     <tbody class="bg-white">
-                                    <tr class="text-gray-700">
+                                        <tr class="text-gray-700">
                                             <td class="w-1/2 px-1 py-2">
                                                 <div class="flex items-center text-sm">
                                                     <div class="relative w-10 h-10 mr-3 rounded-full md:block">
@@ -269,7 +281,7 @@
                                                 </div>
                                             </td>
                                             <td class="w-1/2 px-1 py-5 text-xs text-right text-red-500">
-                                            <?php include '../components/dashboard/rating.php'; ?>
+                                                <?php include '../components/dashboard/rating.php'; ?>
                                             </td>
                                         </tr>
                                         <tr class="text-gray-700">
@@ -286,7 +298,7 @@
                                                 </div>
                                             </td>
                                             <td class="w-1/2 px-1 py-5 text-xs text-right text-red-500">
-                                            <?php include '../components/dashboard/rating.php'; ?>
+                                                <?php include '../components/dashboard/rating.php'; ?>
                                             </td>
                                         </tr>
                                         <tr class="text-gray-700">
@@ -303,7 +315,7 @@
                                                 </div>
                                             </td>
                                             <td class="w-1/2 px-1 py-5 text-xs text-right text-red-500">
-                                            <?php include '../components/dashboard/rating.php'; ?>
+                                                <?php include '../components/dashboard/rating.php'; ?>
                                             </td>
                                         </tr>
                                         <tr class="text-gray-700">
@@ -320,7 +332,7 @@
                                                 </div>
                                             </td>
                                             <td class="w-1/2 px-1 py-5 text-xs text-right text-red-500">
-                                            <?php include '../components/dashboard/rating.php'; ?>
+                                                <?php include '../components/dashboard/rating.php'; ?>
                                             </td>
                                         </tr>
                                         <tr class="text-gray-700">
@@ -337,10 +349,10 @@
                                                 </div>
                                             </td>
                                             <td class="w-1/2 px-1 py-5 text-xs text-right text-red-500">
-                                            <?php include '../components/dashboard/rating.php'; ?>
+                                                <?php include '../components/dashboard/rating.php'; ?>
                                             </td>
                                         </tr>
-                                        
+
                                     </tbody>
                                 </table>
                             </div>
